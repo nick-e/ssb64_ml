eb558e256fa3ed934f7f18ebacb7ff869cd80c82
Nick <nickscotteaton@gmail.com>
2018-01-29 19:56:25 -0800

    Get training batches from the training data files

--- train.py
+++ train.py
@@ -2,6 +2,22 @@ import numpy as np
 import cv2
 import os
 import tensorflow as tf
+import struct
+
+def controllerBinaryToNumpy(binary):
+	controllerNp = np.empty(11)
+	controllerNp[0] = 1 if (binary[0] & 0x01) else 0
+	controllerNp[1] = 1 if (binary[0] & 0x02) else 0
+	controllerNp[2] = 1 if (binary[0] & 0x04) else 0
+	controllerNp[3] = 1 if (binary[0] & 0x08) else 0
+	controllerNp[4] = 1 if (binary[0] & 0x0f) else 0
+	controllerNp[5] = 1 if (binary[0] & 0x10) else 0
+	controllerNp[6] = 1 if (binary[0] & 0x20) else 0
+	controllerNp[7] = struct.unpack('f', binary[1:5])[0]
+	controllerNp[8] = struct.unpack('f', binary[5:9])[0]
+	controllerNp[9] = struct.unpack('f', binary[9:13])[0]
+	controllerNp[10] = struct.unpack('f', binary[13:17])[0]
+	return controllerNp
 
 def main():
 	video_input_layer = tf.layers.Input(
@@ -58,12 +74,31 @@ def main():
 		units = 11
 	)
 
+	# that is 960 * 540 * 3 * 10
+	# since it's so huge, I'm not going to put it in the loop where it constantly needs to be reinitialized
+	videoFrames = np.empty(15552000)
+	for filename in os.listdir('data/'):
+		if filename.endswith('.mp4'):
+			prefix = filename.replace('.mp4', '')
+			print("training " + filename)
+			videoFile = cv2.VideoCapture('data/' + filename)
+			controllerFile = open('data/' + prefix + '.cont', 'rb')
 
-	#for filename in os.listdir('/data'):
-	#	if filename.endswith('.avi'):
-	#		prefix = filename.replace('.avi', '')
-	#		videoFile = cv2.VideoCapture(filename)
-	#		controllerFile = open('/data' + filename.replace('.avi', '') + '.cont', 'rb')
+			while True:
+				controllerBinaries = controllerFile.read(170)
+				if len(controllerBinaries) != 170:
+					break
+				controllerInputs = np.empty(110)
+				for i in range(10):
+					controllerNp = controllerBinaryToNumpy(controllerBinaries[i * 17:(i + 1) * 17])
+					controllerInputs[i * 11:(i + 1) * 11] = controllerNp
+
+				for i in range(10):
+					ret, frame = videoFile.read()
+					frame = frame.flatten()
+					videoFrames[i * 1555200:(i + 1) * 1555200] = frame
+
+			videoFile.release()
 
 
 if __name__ == '__main__':
